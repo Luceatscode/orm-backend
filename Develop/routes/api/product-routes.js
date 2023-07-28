a0d1ae4836ed5471f104a3cbb536a9df0ed9b8c2
Luceatscode <lucsw92@hotmail.com>
2023-07-28 17:12:37 -0600

    created routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -3,16 +3,24 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({ include: [Category, Tag] })
+    .then((productData) => {
+      res.json(productData);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
-// get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findAll({ include: [Category, Tag] })
+    .then((productData) => {
+      res.json(productData);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 // create new product
@@ -93,7 +101,13 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Category.destroy({ where: { id: req.params.id } })
+    .then((productData) => {
+      res.json(productData);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
